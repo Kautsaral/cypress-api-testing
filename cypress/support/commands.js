cb53ddb2cbb3dacabdeabdcb415ff00d01059830
Kautsaral <kautsaral84@gmail.com>
2026-09-22 22:47:22 +0700

    test: improve booking API tests and fixtures

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,78 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add("createBooking", (bookingData) => {
+  return cy.request({
+    method: "POST",
+    url: "/booking",
+    body: bookingData,
+  });
+});
+
+Cypress.Commands.add("getBooking", (bookingId, failOnStatusCode = true) => {
+  return cy.request({
+    method: "GET",
+    url: `/booking/${bookingId}`,
+    failOnStatusCode: failOnStatusCode,
+  });
+});
+
+Cypress.Commands.add("updateBooking", (bookingId, authToken, bookingData) => {
+  return cy.request({
+    method: "PUT",
+    url: `/booking/${bookingId}`,
+    headers: {
+      Cookie: `token=${authToken}`,
+      Accept: "application/json",
+    },
+    body: bookingData,
+  });
+});
+
+Cypress.Commands.add("deleteBooking", (bookingId, authToken) => {
+  return cy.request({
+    method: "DELETE",
+    url: `/booking/${bookingId}`,
+    headers: {
+      Cookie: `token=${authToken}`,
+      Accept: "application/json",
+    },
+  });
+});
+Cypress.Commands.add("getAuthToken", () => {
+  return cy
+    .env(["API_USERNAME", "API_PASSWORD"])
+    .then(({ API_USERNAME, API_PASSWORD }) => {
+      return cy
+        .request({
+          method: "POST",
+          url: "/auth",
+          body: {
+            username: API_USERNAME,
+            password: API_PASSWORD,
+          },
+        })
+        .then((response) => {
+          expect(response.status).to.eq(200);
+          expect(response.body).to.have.property("token");
+
+          return response.body.token;
+        });
+    });
+});
+Cypress.Commands.add("updateBookingWithoutAuth", (bookingId, bookingData) => {
+  return cy.request({
+    method: "PUT",
+    url: `/booking/${bookingId}`,
+    failOnStatusCode: false,
+    body: bookingData,
+  });
+});
+
+Cypress.Commands.add("deleteBookingWithoutAuth", (bookingId) => {
+  return cy.request({
+    method: "DELETE",
+    url: `/booking/${bookingId}`,
+    failOnStatusCode: false,
+  });
+});
